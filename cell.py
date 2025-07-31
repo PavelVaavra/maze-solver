4da08f077afe69246ff5c5cc24db8821ec06be6c
PavelVaavra <pavelvavra.vm@gmail.com>
2025-07-31 15:44:36 +0200

    cell class

--- cell.py
+++ cell.py
@@ -15,3 +15,33 @@ class Line():
             self.point2.x, self.point2.y, 
             fill=fill_color, width=2
         )
+
+class Cell():
+    def __init__(self, window):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self.__x1 = -1
+        self.__x2 = -1
+        self.__y1 = -1
+        self.__y2 = -1
+        self.__win = window
+
+    def draw(self, x, y, cell_size_x=50, cell_size_y=50):
+        self.__x1 = x
+        self.__x2 = x + cell_size_x
+        self.__y1 = y
+        self.__y2 = y + cell_size_y
+        p1 = Point(self.__x1, self.__y1)
+        p2 = Point(self.__x2, self.__y1)
+        p3 = Point(self.__x2, self.__y2)
+        p4 = Point(self.__x1, self.__y2)
+        if self.has_top_wall:
+            self.__win.draw_line(Line(p1, p2))
+        if self.has_right_wall:
+            self.__win.draw_line(Line(p2, p3))
+        if self.has_bottom_wall:
+            self.__win.draw_line(Line(p3, p4))
+        if self.has_left_wall:
+            self.__win.draw_line(Line(p4, p1))
--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from cell import Line, Point
+from cell import Cell
 from window import Window
 
 def main():
     win = Window(800, 600)
-    line1 = Line(Point(10, 10), Point(790, 590))
-    win.draw_line(line1, "red")
-    line2 = Line(Point(10, 590), Point(790, 10))
-    win.draw_line(line2)
+    c = Cell(win)
+    c.draw(20, 30)
     win.wait_for_close()
 
 if __name__ == "__main__":
