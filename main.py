a7ca762c43aaba26e5838aff0481d187b3df37a7
PavelVaavra <pavelvavra.vm@gmail.com>
2025-08-02 07:23:34 +0200

    define maze with cell size and number of rows and collumns

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from window import Window
 from maze import Maze
 
 def main():
-    win = Window(800, 600)
-    m = Maze(40, 40, 13, 18, win)
+    cell_size = 40
+    num_rows = 13
+    num_cols = 18
+    win = Window((num_cols + 2) * cell_size, (num_rows + 2) * cell_size)
+    m = Maze(cell_size, cell_size, num_rows, num_cols, win, cell_size, cell_size)
     win.wait_for_close()
 
 if __name__ == "__main__":
